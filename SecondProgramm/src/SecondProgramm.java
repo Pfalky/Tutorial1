77dcf29d570073fdd05c81806c0cb41d6bd8f992
Pfalky <Pfalky@yandex.ru>
2016-10-25 11:07:48 +0300

    second Programm

--- SecondProgramm/src/SecondProgramm.java
+++ SecondProgramm/src/SecondProgramm.java
@@ -2,4 +2,11 @@
  * Created by Vladimir on 25.10.2016.
  */
 public class SecondProgramm {
+    public static void main(String[] args) {
+        int num;
+        num = 100;
+        System.out.println("Это переменная Num "+num);
+        num=num*2;
+        System.out.println("Переменная Num * 2 равно: "+num);
+    }
 }
