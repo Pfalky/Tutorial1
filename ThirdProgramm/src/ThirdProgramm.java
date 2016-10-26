31c2f1eb5f776d1b068bc7455d9c2213f9e54c32
Pfalky <Pfalky@yandex.ru>
2016-10-26 11:57:28 +0300

    third listening

---
+++ ThirdProgramm/src/ThirdProgramm.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Vladimir on 25.10.2016.
+ */
+public class ThirdProgramm {
+    public static void main(String[] args) {
+        int x, y;
+        x = 10;
+        y = 20;
+        if (x < y) System.out.println("X меньше Y");
+        x = x * 2;
+        if (x == y) System.out.println("Теперь X равно Y");
+        x = x * 2;
+        if (x > y) System.out.println("Теперь X больше Y");
+    }
+}
