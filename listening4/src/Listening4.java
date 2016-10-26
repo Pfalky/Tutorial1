3eb7fea35f6c54e76cb3e6b4743adc2da912d335
Pfalky <Pfalky@yandex.ru>
2016-10-26 12:42:10 +0300

    light distance

---
+++ listening4/src/Listening4.java
@@ -0,0 +1,20 @@
+/**
+ * Created by Vladimir on 26.10.2016.
+ */
+public class Listening4 {
+    public static void main(String[] args) {
+        int lightspeed;
+        long days;
+        long seconds;
+        long distance;
+
+        lightspeed = 186000;
+        days = 1000;
+        seconds = days*24*60*60;
+        distance = lightspeed * seconds;
+
+        System.out.println("За " + days);
+        System.out.println(" дней свет пройдет около ");
+        System.out.println(distance + " миль.");
+    }
+}
