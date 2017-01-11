b1518dbf29113a1cf846a1fabc491e8f9a9e9292
pfalky <pfalky@yandex.ru>
2017-01-11 21:52:29 +0300

    test from home

--- Unit1/listening8/src/listening8.java
+++ Unit1/listening8/src/listening8.java
@@ -18,6 +18,6 @@ public class listening8 {
         System.out.println("b = 0x"+hex[b>>4 & 0x0f] + hex[b&0x0f]);
         System.out.println("b>>4 = 0x"+hex[c>>4 & 0x0f] + hex[c&0x0f]);
         System.out.println("b>>>4 = 0x"+hex[d>>4 & 0x0f] + hex[d&0x0f]);
-        System.out.println("b&0x0f>>4 = 0x"+hex[e>>4 & 0x0f] + hex[e&0x0f]);
+        System.out.println("b&0x0f>>4 = 0x"+hex[e>>4 & 0x0f] + hex[e&0x0f]); // test
     }
 }
